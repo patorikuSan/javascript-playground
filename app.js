45fd26ffb14759323137d7c127eef648d5854090
Patrick Obra <patrick.obra@gmail.com>
2022-05-29 00:15:44 +0800

    first js push 2022

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 window.onload = () => {
-    //console.log('This is your first program executing!');
+    console.log('This is your first program executing!');
+	console.log('getting the hang like before');
+	//Executing javascript code
 }
