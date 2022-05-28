0e5489f763c0305770eba12a1eaacc9061631b78
Patrick Obra <patrick.obra@gmail.com>
2022-05-29 01:12:50 +0800

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 window.onload = () => {
+	let arr =[];
+
+	const indexVal = 10;
+	for(let i=0; i<=indexVal; i++){
+		if(i % 2 == 0){
+			arr.push(i);
+		}
+	}
+	console.log(arr);
     console.log('This is your first program executing!');
 	console.log('getting the hang like before');
 	//Executing javascript code
